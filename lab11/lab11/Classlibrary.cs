9c4b3f08b222ee3d59640539c54565349802b49d
urushi <krasavaagadaok@gmail.com>
2024-12-27 11:39:08 +0300

    Add files via upload

---
+++ lab11/lab11/Classlibrary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11
+{
+    // Интерфейс для печати
+    public interface IPrintable
+    {
+        void Print();
+    }
+
+    // Класс, представляющий документ
+    public class Document : IPrintable
+    {
+        public string Content { get; set; }
+
+        public Document(string content)
+        {
+            Content = content;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Printing document: " + Content);
+        }
+    }
+
+    // Класс, представляющий отчет
+    public class Report : IPrintable
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public Report(string title, string body)
+        {
+            Title = title;
+            Body = body;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Printing report: " + Title);
+            Console.WriteLine("Body: " + Body);
+        }
+    }
+
+
+
+}
---
+++ lab11/lab11/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab11
+{
+
+    public partial class Form1 : Form
+    {
+        private Button btnPrint;
+        public Form1()
+        {
+            // Инициализация элементов управления
+            btnPrint = new Button();
+            btnPrint.Text = "Print";
+            btnPrint.Click += btnPrint_Click;
+            btnPrint.Dock = DockStyle.Fill;
+
+            Controls.Add(btnPrint);
+        }
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            // Создаем объекты для различных типов, реализующих интерфейс IPrintable
+            IPrintable document = new Document("This is a sample document.");
+            IPrintable report = new Report("Annual Report", "This is the body of the report.");
+
+            // Печатаем каждый объект
+            document.Print();
+            report.Print();
+        }
+    }
+}
